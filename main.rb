d98fe2fe6e275810fbe3d4e87050f88791c130e7
William Roy <willjoroy@gmail.com>
2016-09-24 11:14:36 +0100

    Cleaned up code a bit and made a buttonstate function for future button compatibility

--- main.rb
+++ main.rb
@@ -53,32 +53,33 @@ class MenuWindow < Gosu::Window
         @Menu_background = Gosu::Image.new("Resources/MenuBack.png")
         @cursor = Gosu::Image.new(self, 'Resources/cursor.png')
         @button1 = Gosu::Image.new(self, 'Resources/button1.png')
-        @button1active = Gosu::Image.new(self, 'Resources/button1active.png')
+        @button1active = Gosu::Image.new(self, 'Resources/button1a.png')
         @button1state = false 
     end
     def update
         puts("X: #{self.mouse_x} Y: #{self.mouse_y}") if Gosu::button_down? Gosu::KbX
-        if self.mouse_x > 472 and self.mouse_x < (472 + @button1.width)
-            if self.mouse_y > 22 and self.mouse_y < (22 + @button1.height)
-                if Gosu::button_down? Gosu::MsLeft
-                    @button1state = true
-                else
-                    @button1state = false
-                end
-            else
-                @button1state = false
-            end
-        else
-            @button1state = false
-        end
     end
     def draw
         @Menu_background.draw(0, 0, 0);
-        @button1.draw(472, 22, 1) if @button1state == false
-        @button1active.draw(472, 22, 1) if @button1state == true
-        @cursor.draw(self.mouse_x, self.mouse_y, 2)
+        @button1.draw(472, 5, 1) if buttonstate(@button1, 472, 5) == false
+        @button1active.draw(472, 5, 1) if buttonstate(@button1, 472, 5) == true
+        @cursor.draw(self.mouse_x, self.mouse_y, 2) 
     end
-
+    def buttonstate(button, coordsx, coordsy)
+        if self.mouse_x > coordsx and self.mouse_x < (coordsx + button.width)
+            if self.mouse_y > coordsy and self.mouse_y < (coordsy + button.height)
+                if Gosu::button_down? Gosu::MsLeft
+                    true
+                else
+                    false
+                end
+            else
+                false
+            end
+        else
+            false
+        end
+    end
     def button_down(id)
         case id
             when Gosu::KbEscape
